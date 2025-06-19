0cc7b6bdce6c7e53d5233730b68db0e97e56c79d
Mehtab918 <khanmehtab4@gmail.com>
2025-06-19 10:48:29 +0000

    comparisions

--- 01-basics/03_conversions-datatypes.js
+++ 01-basics/03_conversions-datatypes.js
@@ -1,4 +1,5 @@
 
+//String conversion
 let a = "33";
 console.log(a);     //33
 console.log(typeof a );     //string
@@ -26,6 +27,8 @@ let score = null;
 console.log(typeof (score));
 console.log(typeof score);
 
+//Boolean conversion
+
 let isLoggedIn = 1;
 console.log(Boolean(isLoggedIn)); // true
 
@@ -37,3 +40,62 @@ console.log(Boolean(isLoggedIn)); // false
 //"33" -> Number(33);  
 // null -> typeof null -> Object
 //undefined -> typeof undefined -> undefined.
+
+//Number conversion
+let val = undefined;
+console.log(Number(val));
+
+val = null;
+console.log(Number(null));
+
+let str = "     432      ";
+console.log(Number(str));
+
+
+// Notes:
+/*  widely used conversion:  1) number to String/ boolean to String :  String(value)
+                             2) string to number/ boolean to number :  Number(value)
+                             3) string to boolean/number to boolean :  Boolean(value)
+
+*/
+
+
+// *************************OPERATIONS***********************************
+
+// let value1 = 10;
+// value1 = -value1;
+// console.log(value1);
+
+// let value2 = 2;
+// console.log(value1 + value2);
+// console.log(value1 - value2);
+// console.log(value1 * value2);
+// console.log(value1/ value2);
+// console.log(value1 % value2);
+// console.log(value1 ** value2); //10 to the power of 2 = 100
+
+//string concatenation 
+// let s1 = "hello" + "world";
+// console.log(s1);
+
+// let s2 = "1" + 2 ;
+// console.log(s2);  //12
+
+// let s3 = 2 + "1" + 4
+// console.log(s3);  //214
+
+//increment
+//post prefix(value++)
+
+// first print and then increment.
+let valueOne = 10;
+console.log (`value: ${valueOne++}`); // 10 print and then in background the value of valueOne changes to 11.
+console.log (valueOne++);  // this will show you 11.
+
+//pre prefix (++value)
+//first increment then value print.
+
+let counter = 1;
+console.log (++counter); //2
+// console.log (++counter);
+
---
+++ 01-basics/04_comparisions.js
@@ -0,0 +1,25 @@
+
+//comparision
+//-------------------------------------------
+ console.log("===========Avoid to make this comparision . Please make the code clean using strictly equality operator==============");
+ 
+// console.log("2"> 1);
+console.log( true > 0);
+console.log(false < 1);
+console.log(null == undefined);
+console.log("02" > 1);
+
+console.log(null > 0); // false 
+console.log(null >=0); // true 
+console.log(null == 0);
+console.log(undefined == 0);
+
+
+// Strictly equality operator (===)  - IQ 
+// A strict equality operator === checks the equality without type conversion
+
+console.log("2" === 2); //false
+
+
+
+
